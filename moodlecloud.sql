f7d9bc9562cc475eab34c6cfa808d1b4fb6b9659
sudarko <darko@unej.ac.id>
2020-04-15 10:30:47 +0700

    update moodlecloud.sql

--- moodlecloud.sql
+++ moodlecloud.sql
@@ -23,7 +23,7 @@ SET row_security = off;
 CREATE DATABASE moodlecloud WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
 
 
-ALTER DATABASE moodlecloud OWNER TO postgres;
+ALTER DATABASE moodlecloud OWNER TO moodle;
 
 \connect moodlecloud
 
